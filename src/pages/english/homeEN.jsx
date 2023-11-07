045ef8d7de91189cae10babe622bf2427a9a475b
old7evenn <stepanukroman9@gmail.com>
2023-11-07 23:32:54 +0200

    add header

--- src/pages/english/homeEN.jsx
+++ src/pages/english/homeEN.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { HeaderEN } from "../../components/header/HeaderEN";
 
 export const HomeEN = () => {
   return (
     <>
-      <h2>Hello this is homeEN</h2>
-      <h3>Hello this is homeEN</h3>
-      <button>
-        <Link to="/de">to homeDE</Link>
-      </button>
-      <button>
-        <Link to="/en/organicShop">to organicShop</Link>
-      </button>
+			<HeaderEN />
     </>
-  );
+	)
 };
