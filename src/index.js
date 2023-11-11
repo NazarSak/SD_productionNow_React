461bf2e24e386e1e7a209284d0af3cdab4061e45
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-11-11 19:05:23 +0200

    remake

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./technical/reportWebVitals";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/english/organicShopEN.jsx
+++ src/pages/english/organicShopEN.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { HeaderEN } from "../../components/header/HeaderEN";
 
 export const OrganicShopEN = () => {
   return (
     <>
+      <HeaderEN />
       <h2>hello this organicShopEN</h2>
       <h3>hello this organicShopEN</h3>
       <button>
